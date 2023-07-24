33011dec2e7a5f0856b79ac1a084a0dd89edee25
BayuDC <dev.bayudc@gmail.com>
2023-07-24 17:45:10 +0700

    chore: setup styling var

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        <NuxtWelcome />
-    </div>
+    <Html>
+        <Head>
+            <Title>Waifuseum</Title>
+            <Link rel="preconnect" href="https://fonts.googleapis.com" />
+            <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+            <Link
+                href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;0,600;0,700;1,700&display=swap"
+                rel="stylesheet"
+            />
+        </Head>
+        <Body class="bg-white text-black">
+            <h1 class="font-bold italic">Waifuseum</h1>
+        </Body>
+    </Html>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,16 @@
 import type { Config } from 'tailwindcss';
 
 export default {
+    theme: {
+        fontFamily: {
+            sans: ['Fira Sans', 'sans-serif'],
+        },
+        colors: {
+            black: '#212738',
+            white: '#f6f8ff',
+            green: '#23ce6b',
+            gray: '#606677',
+        },
+    },
     content: [],
 } as Config;
