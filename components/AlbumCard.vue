e946b467cc4aa95ad053aa9840e3c8c9bf881a15
BayuDC <dev.bayudc@gmail.com>
2023-07-24 22:28:07 +0700

    feat: lazy album thumbnail

--- components/AlbumCard.vue
+++ components/AlbumCard.vue
@@ -4,7 +4,7 @@ defineProps<{ album: Album }>();
 
 <template>
     <div class="p-4 bg-white rounded-2xl border-black border-4 h-full shadow-[4px_4px_0px_0px_black]">
-        <PictureThumb :urls="album.pictures.map((p) => p.urls.thumbnail)" />
+        <PictureThumb :url="album.pictures[2].urls.thumbnail" />
         <h3 class="font-bold italic text-lg mt-2">{{ album.name }}</h3>
     </div>
 </template>
--- components/PictureThumb.vue
+++ components/PictureThumb.vue
@@ -1,12 +1,34 @@
 <script lang="ts" setup>
+import { Icon } from '@iconify/vue';
+import { useLazyload } from 'vue3-lazyload';
+
 const props = defineProps<{
-    urls: string[];
+    url: string;
 }>();
+
+const ready = ref(false);
+const element = useLazyload(toRef(props, 'url'), {
+    lifecycle: {
+        loaded: () => {
+            ready.value = true;
+        },
+    },
+    observerOptions: {
+        rootMargin: '200px',
+    },
+});
 </script>
 
 <template>
-    <div class="overflow-hidden rounded-md aspect-square">
-        <img :src="urls[0]" class="w-full h-full object-cover object-top" />
+    <div class="overflow-hidden rounded-md aspect-square bg-green/20">
+        <div v-if="!ready" class="text-green h-full flex items-center justify-center">
+            <Icon icon="line-md:loading-twotone-loop" :width="54" :height="54" />
+        </div>
+        <img
+            class="w-full h-full object-cover object-top transition duration-300"
+            :class="[ready ? 'opacity-100' : 'opacity-0 blur']"
+            ref="element"
+        />
     </div>
 </template>
 
