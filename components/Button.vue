8671a13205e7decf5b27889d3eed8264dfc99363
BayuDC <dev.bayudc@gmail.com>
2023-07-24 19:01:52 +0700

    feat: the button component

---
+++ components/Button.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+    <NuxtLink to="#" class="font-bold text-black bg-green py-2 px-4 rounded-md block hover:scale-105 transition">
+        <slot />
+    </NuxtLink>
+</template>
+
+<style scoped></style>
--- components/Header.vue
+++ components/Header.vue
@@ -3,8 +3,11 @@
 <template>
     <header class="bg-black">
         <div class="container mx-auto max-w-screen-xl px-5 md:px-10">
-            <div class="flex items-center h-20">
-                <h1 class="font-bold italic text-green text-[32px]">Waifuseum</h1>
+            <div class="flex items-center justify-between h-20">
+                <h1 class="font-bold text-green italic text-[32px]">Waifuseum</h1>
+                <div>
+                    <Button>Contribute</Button>
+                </div>
             </div>
         </div>
     </header>
