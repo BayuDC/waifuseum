7c16912f54f35c53874878eeac32a7b45d1dfd35
BayuDC <dev.bayudc@gmail.com>
2023-07-28 05:17:54 +0700

    feat: favicon?

--- app.vue
+++ app.vue
@@ -14,6 +14,9 @@ useNuxtApp().hook('page:finish', () => {
                 href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;0,600;0,700;1,700&family=Inter:wght@700&display=swap"
                 rel="stylesheet"
             />
+            <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         </Head>
         <Body class="bg-white text-black">
             <NuxtLayout>
---
+++ pages/upload.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+throw showError({
+    statusCode: 425,
+    statusMessage: 'Coming soon',
+});
+</script>
