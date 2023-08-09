ec4cf83c73e8802a4a03292fde21e9361dda52e3
BayuDC <dev.bayudc@gmail.com>
2023-08-10 01:27:36 +0700

    feat: input base component

--- pages/dashboard/create-tag.vue
+++ pages/dashboard/create-tag.vue
@@ -3,7 +3,12 @@
 <template>
     <Section title="Create Tag" no-padding>
         <Box>
-            <div class="">Coming soon!</div>
+            <div class="grid md:grid-cols-2 gap-x-3 mb-4">
+                <InputBase state="" type="text" button="" />
+                <InputBase state="loading" type="text" />
+                <InputBase state="error" type="text" />
+                <InputBase state="success" type="text" />
+            </div>
         </Box>
     </Section>
 </template>
