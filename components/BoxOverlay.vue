c8f5f024deb04e220b277f3d3a114f6b0c5d6b01
BayuDC <dev.bayudc@gmail.com>
2023-08-11 07:23:58 +0700

    feat: handle delete picture

---
+++ components/BoxOverlay.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div class="backdrop-blur-md absolute inset-0 z-20 flex items-center justify-center text-center">
+        <div class="font-bold text-2xl"><slot /></div>
+    </div>
+</template>
+
+<style scoped></style>
--- pages/pictures/[id].vue
+++ pages/pictures/[id].vue
@@ -5,15 +5,41 @@ definePageMeta({
 
 const user = useAuth();
 const id = useRoute().params.id;
-// const mode = ref<'idle' | 'update' | 'delete'>('idle');
-const updateMode = ref(false);
-const deleteMode = ref(false);
 
 const { data: picture, error } = await useLiteFetch<Picture>(`/pictures/${id}`, {
     transform: (d: any) => d.picture,
 });
 
 if (!picture.value) throw showErrorSimplify(error);
+
+const showTools = ref(true);
+
+const deleteMode = ref(false);
+const deleteMessage = ref('');
+const deleteFetch = useMainFetch(`/pictures/${id}`, {
+    key: 'picture-delete',
+    method: 'delete',
+    immediate: false,
+});
+async function onDelete() {
+    showTools.value = false;
+    deleteMessage.value = 'Deleting...';
+
+    await deleteFetch.execute();
+    if (deleteFetch.error.value) {
+        deleteMessage.value = 'Delete Failed';
+        setTimeout(() => {
+            deleteMessage.value = '';
+            deleteMode.value = false;
+            showTools.value = true;
+        }, 3000);
+    } else {
+        deleteMessage.value = 'Deleted';
+    }
+}
+
+const updateMode = ref(false);
+function onUpdate() {}
 </script>
 
 <template>
@@ -21,7 +47,7 @@ if (!picture.value) throw showErrorSimplify(error);
         <Section>
             <div class="grid md:grid-cols-[320px_1fr] gap-4 items-start">
                 <div class="md:sticky md:top-[144px] order-1 md:order-none flex flex-col gap-4">
-                    <Box>
+                    <Box class="relative overflow-hidden">
                         <ul class="flex flex-col gap-4">
                             <InfoCell name="Id" :value="picture.id" />
                             <InfoCell
@@ -42,29 +68,37 @@ if (!picture.value) throw showErrorSimplify(error);
                                 :value="usePrettyDate(picture.createdAt) + ' ' + usePrettyTime(picture.createdAt)"
                             />
                         </ul>
+                        <Transition name="blur">
+                            <BoxOverlay v-if="deleteMessage" :class="{ 'bg-pink/60': deleteMode }">
+                                {{ deleteMessage }}
+                            </BoxOverlay>
+                        </Transition>
                     </Box>
 
-                    <template v-if="user && user.id == picture.createdBy.id">
-                        <Transition name="blur" mode="out-in">
-                            <Box v-if="updateMode">
-                                <InputText label="Source" button="Save" />
-                            </Box>
-                            <div v-else class="flex justify-end">
-                                <Button @click="updateMode = true" icon="ic:round-edit"> Update Source </Button>
-                            </div>
-                        </Transition>
-                        <div class="flex justify-end">
+                    <Transition name="slide">
+                        <div
+                            class="flex flex-col items-end gap-4"
+                            v-if="user && showTools && user.id == picture.createdBy.id"
+                        >
                             <Transition name="blur" mode="out-in">
-                                <Button v-if="!deleteMode" @click="deleteMode = true" icon="ic:round-delete" danger
+                                <Box v-if="updateMode" class="w-full">
+                                    <InputText label="Source" button="Save" />
+                                </Box>
+                                <Button v-else @click="updateMode = true" icon="ic:round-edit">Update Source</Button>
+                            </Transition>
+                            <Transition name="blur" mode="out-in">
+                                <Button v-if="deleteMode" @click="onDelete" icon="ic:round-delete-forever" danger
+                                    >Are you sure?</Button
+                                >
+                                <Button v-else @click="deleteMode = true" icon="ic:round-delete" danger
                                     >Delete This</Button
                                 >
-                                <Button v-else @click="" icon="ic:round-delete-forever" danger>Are you sure?</Button>
                             </Transition>
                         </div>
-                    </template>
+                    </Transition>
                 </div>
                 <div>
-                    <Box>
+                    <Box class="">
                         <div class="border-green border-[4px] rounded-lg overflow-hidden bg-green/20">
                             <img class="w-full" :src="picture.urls.standard" />
                         </div>
